abaebefd96c904e2ab0c8319c92f8d7b424d7188
qazedx <qazedxms@gmail.com>
2015-03-30 20:47:13 +0300

    tdy (tidy up); geolocation via html5 option added; bs lib; external style.css; jq cdn

--- public_html/js1.js
+++ public_html/js1.js
@@ -13,7 +13,26 @@ function initialize() {
         mapTypeId: 'terrain'
     }
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    // Try HTML5 geolocation
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = new google.maps.LatLng(position.coords.latitude,
+                    position.coords.longitude);
 
+            var infowindow = new google.maps.InfoWindow({
+                map: map,
+                position: pos,
+                content: 'Location found using HTML5.'
+            });
+
+            map.setCenter(pos);
+        }, function () {
+            handleNoGeolocation(true);
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+    }
     // Create an ElevationService.
     elevator = new google.maps.ElevationService();
     google.maps.event.addListener(map, 'click', function (e) {
@@ -117,5 +136,25 @@ function plotElevation(results, status) {
         titleY: 'Elevation (m)'
     });
 }
+
+
+
+function handleNoGeolocation(errorFlag) {
+    if (errorFlag) {
+        var content = 'Error: The Geolocation service failed.';
+    } else {
+        var content = 'Error: Your browser doesn\'t support geolocation.';
+    }
+
+    var options = {
+        map: map,
+        position: new google.maps.LatLng(60, 105),
+        content: content
+    };
+
+    var infowindow = new google.maps.InfoWindow(options);
+    map.setCenter(options.position);
+}
+
 
 google.maps.event.addDomListener(window, 'load', initialize);
